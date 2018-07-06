c12932b8b62e97152764ee7c0f1260cceeadda74
Evandro Jr <evandrojr@gmail.com>
2018-07-06 09:28:44 -0300

    entidates_atendimento_gratuito.txt

--- src/providers/local-service/local-service.ts
+++ src/providers/local-service/local-service.ts
@@ -18,6 +18,7 @@ export class LocalServiceProvider {
 
   obterEstabelecimentos(): any {
    
+    // Tipos de serviço
     let s = {
       "i": "Psicoterapia individual",
       "g": "Psicoterapia de Grupo",
@@ -28,20 +29,35 @@ export class LocalServiceProvider {
       "ga": "Grupo para adolescentes"
     }
 
+
+
     let estabelecimentos = [
+
+      // Modelo:
+      {
+        nome: "",
+        end: "",
+        tel: "",
+        valor: "",
+        servico: [,],
+        mapa: ''
+      },
+
+
       {
         nome: "Instituto de Saúde da Faculdade Unijorge",
         end: "Av. Luís Viana Filho, 6775, Paralela",
         tel: "3206-8015",
         valor: "Gratuito",
-        servs: [s.i, s.g]
+        servico: [s.i, s.g],
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.5362927581573!2d-38.41222634920404!3d-12.937497762586544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71610af309d2219%3A0xbc365de20840c79e!2sAv.+Lu%C3%ADs+Viana%2C+6775+-+S%C3%A3o+Marcos%2C+Salvador+-+BA!5e0!3m2!1spt-BR!2sbr!4v1530877497475" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
       },
       {
         nome: "Faculdade da Cidade do Salvador (FACS)",
         end: "Av. Estados Unidos, 37, 3° andar, Comércio",
         tel: "3254-6916/6943",
         valor: "Gratuito",
-        servs: [s.i, s.g, s.gc,]
+        servico: [s.i, s.g, s.gc,]
       },
     ]
     return estabelecimentos;
@@ -55,7 +71,7 @@ export class LocalServiceProvider {
 
 
     estabelecimentos.forEach(estabelecimento => {
-      estabelecimento.servs.forEach(servico => {
+      estabelecimento.servico.forEach(servico => {
          servico = servico.toLowerCase();         
          if(servicoDesejado == servico){
           estabelecimentosEncontrados.push(estabelecimento)
