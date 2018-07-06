6f211388c50706f1285336c0aacbb4581597b090
Evandro Jr <evandrojr@gmail.com>
2018-07-06 11:28:35 -0300

    cadastros bia e nairara

--- src/providers/local-service/local-service.ts
+++ src/providers/local-service/local-service.ts
@@ -183,8 +183,6 @@ export class LocalServiceProvider {
         mapa: ' <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.7056640692804!2d-38.49023994949735!3d-12.990668090798456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71604b468305749%3A0x72a5e386090aeb20!2sBAHIANA+-+School+of+Medicine+and+Public+Health!5e0!3m2!1sen!2sbr!4v1530796297375" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> '
       },
 
-
-      // Modelo:
       {
         nome: "Núcleo de Estudos e Práticas Psicológicas (NEPSI) da Unifacs",
         end: "Rua Solimões, 181, Saboeiro",
@@ -195,8 +193,6 @@ export class LocalServiceProvider {
         mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.0610173228656!2d-38.444615349497695!3d-12.967947390813427!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161a56347e5f53%3A0x2e23aca9bc3c28e1!2sUnifacs+PA+10!5e0!3m2!1sen!2sbr!4v1530796458956" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
       },
 
-
-      // Modelo:
       {
         nome: "FTC - Faculdade de Tecnologia e Ciências",
         end: "Av. Luís Viana Filho, 8812, Paralela",
@@ -205,6 +201,95 @@ export class LocalServiceProvider {
         servico: [s.i],
         descricao_servicos: "Psicodiagnóstico, psicoterapia individual (todas as faixas etárias) e psicoterapia em grupos, família e casal",
         mapa: ' <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.5754021517782!2d-38.39477344949795!3d-12.934988990835148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x716174fae89b33b%3A0x163e53b115d615cd!2sFTC+-+Faculdade+de+Tecnologia+e+Ci%C3%AAncias%2C+Campus+Salvador!5e0!3m2!1sen!2sbr!4v1530796580180" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> '
+      },
+
+      //Naiara
+
+      {
+        nome: "UFBA - Universidade Federal da Bahia",
+        end: "Rua Professor Aristides Novais, 197, Federação (São Lázaro)",
+        tel: "3235-4589",
+        valor: "Gratuito ou Preco Negociado",
+        servico: [s.i, s.p],
+        descricao_servicos: "Atendimento psicológico individual (adolescente, adulto psicodiagnóstico e orientação profissional e idoso)",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.54935361837!2d-38.513638949529465!3d-13.000649963871066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x716049de15a3b17%3A0xe744f5fde0253b33!2sR.+Prof.+Arist%C3%ADdes+Novis%2C+197+-+Federa%C3%A7%C3%A3o%2C+Salvador+-+BA%2C+40210-630!5e0!3m2!1spt-BR!2sbr!4v1530883874947" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+
+      {
+        nome: "Faculdade Ruy Barbosa",
+        end: "Rua Theodomiro Batista, 422, Rio Vermelho",
+        tel: "3205-1745",
+        valor: "Gratuito",
+        servico: [s.i, s.p],
+        descricao_servicos: "Orientação profissional, psicoterapia individual e familiar (todas as faixas etárias), diagnóstico e reabilitação cognitiva.Atendimento psicológico com enfoque em gênero(núcleo de Direitos Humanos e LGBT).",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1943.7040583534754!2d-38.48220960800574!3d-13.009662778361196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161cabb1e48e97%3A0x2980e20c7b0b64ea!2sRua+Theodomiro+Baptista%2C+422+-+Rio+Vermelho%2C+BA%2C+41940-320!5e0!3m2!1spt-BR!2sbr!4v1530884106686" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+
+      {
+        nome: "Instituto de Saúde da Faculdade Unijorge",
+        end: "Av. Luís Viana Filho, 6775, Paralela",
+        tel: "3206-8015",
+        valor: "Gratuito",
+        servico: [s.i, s.g, s.p],
+        descricao_servicos: "Psicoterapia individual a todas as faixas etárias, Psicodiagnóstico, Avaliação Psiconeurológica, Orientação Profissional e Grupos terapêuticos.",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31106.389013382017!2d-38.43724443958639!3d-12.952733188922403!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71610af309d2219%3A0xbc365de20840c79e!2sAv.+Lu%C3%ADs+Viana%2C+6775+-+S%C3%A3o+Marcos%2C+Salvador+-+BA!5e0!3m2!1spt-BR!2sbr!4v1530884231477" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+
+      {
+        nome: "Faculdade da Cidade do Salvador",
+        end: "Av. Estados Unidos, 37, 3° andar, Comércio",
+        tel: "3254-6916/6943",
+        valor: "Gratuito ou Preço Negociado",
+        servico: [s.i, s.g],
+        descricao_servicos: "Psicoterapia individual e em grupo para crianças, adolescentes e adultos.",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.069513768239!2d-38.5115930495299!3d-12.967403663193645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71604e11a5d4391%3A0x3ab90f53e136da6a!2sAv.+Estados+Unidos%2C+37+-+3%C2%B0+andar+-+Comercio%2C+Salvador+-+BA%2C+40010-020!5e0!3m2!1spt-BR!2sbr!4v1530884363209" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+
+      // Beatriz
+      {
+        nome: "Núcleo de Atendimento Psicológico (NAP) da Faculdade Castro Alves/UNIRB",
+        end: "Rua Rubem Berta, 128, Pituba",
+        tel: "3033-0009",
+        valor: "Gratuito",
+        servico: [s.i],
+        descricao_servicos: "Psicoterapia individual para crianças, adolescentes e adultos",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.5721685101134!2d-38.45320374949717!3d-12.999193490792829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x450657f3ff4cc65%3A0x9e7c2345cb524bb0!2sFaculdade+Castro+Alves!5e0!3m2!1spt-BR!2sbr!4v1530882231553" width="600" height="450" frameborder="0" style="border:0" allowfullscreen> </iframe>'
+      },
+      {
+        nome: "FACULDADE SOCIAL DA BAHIA – FSBA",
+        end: "Av. Oceânica, 2717, Ondina",
+        tel: "4009-2937/2935",
+        valor: "Gratuito",
+        servico: [s.i],
+        descricao_servicos: "Psicoterapia individual a todas as faixas etárias",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.395059549233!2d-38.510682749497334!3d-13.01049569078536!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x716036635c57f13%3A0xaabc087e3c266e12!2sFaculdade+Social+da+Bahia!5e0!3m2!1spt-BR!2sbr!4v1530882460413" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+      {
+        nome: "UNIME PARALELA",
+        end: "Rua Jairo Simões, 3172, Imbuí",
+        tel: "3879-9155",
+        valor: "Gratuito",
+        servico: [s.i, s.p],
+        descricao_servicos: "Atendimento psicológico individual(criança, adolescente, adulto e idoso) grupos terapêuticos, orientação para pais, orientação profissional(carreira / coaching, inserção no mercado de trabalho, qualidade de vida no trabalho e saúde mental do/a trabalhador/a)",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.1748632446975!2d-38.43402704949762!3d-12.960659990818193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161a42d714b603%3A0xdf606e5008e75b2b!2sR.+Prof.+Ja%C3%ADro+Sim%C3%B5es%2C+3172+-+Pitua%C3%A7u%2C+Salvador+-+BA%2C+41720-375!5e0!3m2!1spt-BR!2sbr!4v1530882793957" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+      {
+        nome: "Faculdade Estácio",
+        end: "Rua Xingu, 179, Jd Atalaia, Stiep.",
+        tel: "2107-8144",
+        valor: "Gratuito",
+        servico: [s.i, s.f],
+        descricao_servicos: "psicoterapia individual (todas as faixas etárias) e familiar",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.845016404577!2d-38.44905474949743!3d-12.981762790804314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7161b06e3eee8a5%3A0x33988cea9946e2e0!2zRXN0w6FjaW8!5e0!3m2!1spt-BR!2sbr!4v1530883024622" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
+      },
+      {
+        nome: "UNIVERSIDADE DO ESTADO DA BAHIA – UNEB",
+        end: "Rua Silveira Martins, 2555, Cabula",
+        tel: "3117-5336/5341",
+        valor: "Gratuito",
+        servico: [s.i, s.g],
+        descricao_servicos: "psicoterapia individual e em grupo com todas as faixas etárias",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.318776205092!2d-38.46079249843412!3d-12.951442199999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71612308d8d0ea7%3A0xdddaf66ca849c879!2sUniversidade+do+Estado+da+Bahia!5e0!3m2!1spt-BR!2sbr!4v1530883300079" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
       },
     ]
     return estabelecimentos;
