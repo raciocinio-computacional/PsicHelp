0b2a0b7f671f1a9cbd46452c3f94343ba387ac99
Evandro Junior <evandrojr@gmail.com>
2018-07-08 07:10:54 -0300

    scripts de publicação melhorados

--- publicar_webapp.sh
+++ publicar_webapp.sh
@@ -9,23 +9,23 @@ APP="PsicHelp"
 }
 
 
-# ionic cordova platform add browser
-# ionic cordova build browser --prod --release
+ionic cordova platform add browser
+ionic cordova build browser --prod --release
 
-# rm -rf docs
-# cp www docs -r
-# cd docs
-# php -S 0.0.0.0:8015
+rm -rf docs
+cp www docs -r
+cd docs
+php -S 0.0.0.0:8015
 
-# echo Teste primeiro localmente, funcionou?
-# read answer
-# if [ "$answer" != "${answer#[SsYy]}" ] ;then
-#     echo OK, continuando
-# else
-#     echo Saindo!
-#     exit
-# fi
-# cd ..
+echo Teste primeiro localmente, funcionou?
+read answer
+if [ "$answer" != "${answer#[SsYy]}" ] ;then
+    echo OK, continuando
+else
+    echo Saindo!
+    exit
+fi
+cd ..
 
 acp Publicando o webApp 
 echo teste agora em https://bit.ly/psichelpweb funcionou?
