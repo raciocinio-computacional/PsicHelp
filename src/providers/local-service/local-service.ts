f96d2c94d5da7f5ded9edd0741c59f12a88a5a73
Evandro Junior <evandrojr@gmail.com>
2018-07-08 06:12:55 -0300

    mudando cores

--- src/providers/local-service/local-service.ts
+++ src/providers/local-service/local-service.ts
@@ -132,7 +132,8 @@ export class LocalServiceProvider {
         tel: "3021-0500",
         valor: "Gratuito",
         servico: [s.i],
-        descricao_servicos: "Reabilitação, Habilitação e educação de crianças e adolescentes portadores de deficiência mental, Serviço Social, Neurologia, Fonoaudiologia, Psiquiatria, Psicoterapia e Psicopedagogia.         Pagamento: Gratuito credenciado pelo SUS.", mapa: ' Rua Raul Leite, 93, Vila Laura, Matatu de Brotas.'
+        descricao_servicos: "Reabilitação, Habilitação e educação de crianças e adolescentes portadores de deficiência mental, Serviço Social, Neurologia, Fonoaudiologia, Psiquiatria, Psicoterapia e Psicopedagogia.",
+        mapa: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.880474847214!2d-38.50227635092514!3d-12.979495863436535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71604c6c824a1e5%3A0x335b8019a4187f47!2sAssocia%C3%A7%C3%A3o+Baiana+de+Recupera%C3%A7%C3%A3o+do+Excepcional!5e0!3m2!1spt-BR!2sbr!4v1531040998635" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>'
       },
 
       {
