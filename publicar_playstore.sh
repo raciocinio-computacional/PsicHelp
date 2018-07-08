731f8ac024d3e8342495e4ba98176ac8f2b622d2
Evandro Junior <evandrojr@gmail.com>
2018-07-08 10:34:07 -0300

    Publicando o webApp

--- publicar_playstore.sh
+++ publicar_playstore.sh
@@ -12,22 +12,19 @@ else
 fi
 
 echo "Gerando versão de Release para $APP"
-rm $APP.apk
 ionic cordova build android --prod --release
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
+rm -f $APP.apk
+~/Android/Sdk/build-tools/26.0.2/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $APP.apk
+
 echo "Apague a versão antiga e teste primeiro teste no seu celular e dê enter!"
 read answer
 adb install $APP.apk
 echo "Testou e funcinou?!"
 read answer 
 if [ "$answer" != "${answer#[SsYy]}" ] ;then
-    echo OK, continuando
+    echo OK parabêns! Pronto para ir para a loja!
 else
-    echo Saindo!
+    echo Erro saindo!
     exit
 fi
-
-
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
-rm -f $APP.apk
-
-~/Android/Sdk/build-tools/26.0.2/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk $APP.apk
